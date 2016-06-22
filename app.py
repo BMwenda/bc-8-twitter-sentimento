42659414881547e8fd746836a0f2dddece1fd8ac
Desterio D <desteriodennis@live.com>
2016-06-22 15:14:45 +0300

    Modify the fetch_tweets method

--- app.py
+++ app.py
@@ -16,13 +16,16 @@ def save_to_json(data):
 def twitter_fetch(screen_name = "BBCNews", maxnumtweets=200):
     'Fetch tweets from @BBCNews'
 
-    tweets = tweepy.Cursor(api.user_timeline, id=screen_name).items(10)
+    tweets = tweepy.Cursor(api.user_timeline, id=screen_name).items(50)
     for status in tweets:
         print(status.text + "\n")
         user_tweets.append(status.text)
-    save_to_json(user_tweets)
+
+    return user_tweets
+    #save_to_json(user_tweets)
 
   
 
 if __name__ == '__main__':
-    twitter_fetch('@Mwaniki', 50)
+    twitter_fetch('@Mwaniki')
+    save_to_json(user_tweets)

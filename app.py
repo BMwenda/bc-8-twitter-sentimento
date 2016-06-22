d4871a6944f591f8239f351db4d7747fe4492154
Desterio D <desteriodennis@live.com>
2016-06-22 19:01:41 +0300

    get-tweets method in app.py

--- app.py
+++ app.py
@@ -10,14 +10,23 @@ access_token_secret="A6gqoojWr8TYuJMXVDKxb892uM7HJeCoP8b4rkmTi3PZ0"
 
 twitter = Twitter(auth = OAuth(access_token, access_token_secret, consumer_key, consumer_secret))
 
-user = "artbrute"
+user_tweets = {}
 
-results = twitter.statuses.user_timeline(screen_name = user, count=20)
-with open("json_data_file.json", "w") as out_file:
-    json.dump(results, out_file)
+def get_tweets(twitter_handle, max_number_of_tweets):
+    results = twitter.statuses.user_timeline(screen_name = twitter_handle, count=max_number_of_tweets)
 
-with open("json_data.json", "r") as in_file:
-    json.load()
+    return results
 
-for status in results:
-    print("(%s) %s" % (status["created_at"], status["text"].encode("ascii", "ignore")))
+
+def save_to_json(data):
+    with open("json_data_file.json", "w") as out_file:
+        json.dump(data, out_file)
+
+#with open("json_data.json", "r") as in_file:
+    #json.load()
+
+#for status in results:
+    #print("(%s) %s" % (status["created_at"], status["text"].encode("ascii", "ignore")))
+
+#get_tweets("Mwaniki", 15)
+save_to_json(get_tweets("Mwaniki", 15))

1881979590f5006fd445c5de2e43df163952859e
Desterio D <desteriodennis@live.com>
2016-06-23 12:11:27 +0300

    Create list of tweet text working perfecto

--- app.py
+++ app.py
@@ -3,6 +3,11 @@ import config
 from twitter import *
 import time
 import datetime
+from pprint import pprint
+import panda as pd
+import operator
+from collections import Counter
+from nltk.corpus import stopwords
 
 config_file = open("config_dev.json", "r")
 config = json.loads(config_file.read())
@@ -14,32 +19,32 @@ access_token_secret = config["access_token_secret"]
 
 twitter = Twitter(auth = OAuth(access_token, access_token_secret, consumer_key, consumer_secret))
 
-user_tweets = []
-
 def get_tweets(twitter_handle, max_number_of_tweets):
     tweets = twitter.statuses.user_timeline(screen_name = twitter_handle, count=max_number_of_tweets)
     #user_tweets = str(json.loads(tweets))
 
     return tweets
 
-def get_tweets_2(twitter_handle, from_date, to_date):
-    tweets = twitter.statuses.user_timeline(screen_name=twitter_handle, since=from_date, until=to_date)
-    #user_tweets = str(json.loads(tweets))
 
-    return tweets
-
-
-def save_to_json(data, handle):
-    with open("{}_tweets.json".format(handle), "w") as out_file:
+def save_to_json(data):
+    with open("tweets.json", "w") as out_file:
         json.dump(data, out_file)
 
+def read_from_json():
+    json_data = open("tweets.json").read()
 
-#for status in user_tweets:
-    #print("(%s) %s" % (status["id"], status["text"].encode("ascii", "ignore")))
+    data = json.loads(json_data)
+    tweet_list = []
+    for dictionary in data:
+        tweet_list.append(dictionary["text"])
 
-#for status in results:
-    #print("(%s) %s" % (status["created_at"], status["text"].encode("ascii", "ignore")))
+    return tweet_list
 
-#get_tweets("Mwaniki", "2016-01-01", )
-save_to_json(get_tweets_2("Mwaniki", "2016-01-01", "2016-02-02"), "mwaniki")
-#save_to_json(user_tweets)
+
+save_to_json(get_tweets("Mwaniki", 10))
+#print(read_from_json_and_return_text_list())
+
+my_list = read_from_json()
+#print(my_list[0])
+#for text in my_list:
+    #print(text)
